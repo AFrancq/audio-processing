7214422588bee262c34229a1dd1608fa01700bd0
Ash Francq <ash.francq@gmail.com>
2026-04-04 15:17:52 -0500

    soft reset

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,45 @@
-use std::{fs::File, path::Path};
+use std::{
+    fs::File,
+    io::{Error, Read},
+    path::Path,
+};
 
-fn read_audio_file(file_path: &Path) {
-    println!("test");
+struct WavHeader {
+    riff: [u8; 4],
+    size: u32,
+    file_type: [u8; 4],
+    fmt_marker: [u8; 4],
+    subchunk1_size: u32,
+    audio_format: u16,
+    num_channels: u16,
+    sample_rate: u32,
+    byte_rate: u32,
+    block_align: u16,
+    bits_per_sample: u16,
+    data_marker: [u8; 4],
+    subchunk2_size: u32,
 }
 
-fn main() {
-    let file_path = Path::new("data/amibient-wind.wav");
-    read_audio_file(file_path);
+struct WavData {
+    data: Vec<u8>,
+}
+
+struct Wav {
+    header: WavHeader,
+    data: WavData,
+}
+
+fn read_wav_file(file: File) -> Result<Wav, Error> {}
+
+fn read_audio_file(file_path: &Path) -> Result<Vec<u8>, Error> {
+    let mut data: Vec<u8> = vec![];
+    File::open(file_path)?.read_to_end(&mut data)?;
+    Ok(data)
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let file_path = Path::new("./data/ambient-wind.wav");
+    let data: Vec<u8> = read_audio_file(file_path)?;
+    print!("Test");
+    Ok(())
 }
